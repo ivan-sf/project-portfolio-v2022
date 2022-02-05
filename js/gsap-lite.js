cb4602b5a6f2f548ab74f306b08d1d3cb5775bfc
MajorTom <ivansantander2020@gmail.com>
2022-02-05 09:56:11 -0500

    version 1 sin contacto

--- js/gsap-lite.js
+++ js/gsap-lite.js
@@ -1,64 +1,6 @@
-gsap.registerPlugin(ScrollTrigger);
-const models3d = gsap.utils.toArray(".model-3d");
 
 
-// gsap.from(".header",{
-//     opacity:0,
-//     y:'-10vh',
-//     delay:1,
-//     duration:2,
-// })
-
-// gsap.from(".title-banner",{
-//     opacity:0,
-//     x:'80vw',
-//     delay:2,
-//     duration:3,
-// })
-
-// gsap.from(".model-viewer",{
-//     opacity:0,
-//     x:'100vw',
-//     delay:2,
-//     duration:2,
-// })
-
-// gsap.from(".rss",{
-//     opacity:0,
-//     x:'-20vw',
-//     delay:4,
-//     duration:1,
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if (screen.width > 620) {
+if (screen.width < 620) {
     
     var tlBanner = gsap.timeline({
         scrollTrigger:{
@@ -66,33 +8,19 @@ if (screen.width > 620) {
             markers: false,
             start: 'top top',
             end: 'bottom top',
-            scrub: 5,
-            pin: true
+            scrub: true,
         }
     });
 
     tlBanner.to(".title-banner",{
-        x:'-100vw',
-        opacity:0,
-        duration:3,
+        y:'23vh',
+        duration:1,
     })
 
     tlBanner.to(".model-viewer",{
-        x:'-50vw',
-        duration:1,
-    },"<")
-
-    tlBanner.to(".about-me",{
-        x:'-50vw',
-        delay:0.4,
-        duration:3,
-    },"<")
-
-    tlBanner.to(".banner",{
-        opacity:'0',
-        delay:2,
         duration:2,
-    },"<")
+        y:'20vh',
+        },"<")
 
     var tlSubBanner = gsap.timeline({
         scrollTrigger:{
@@ -100,15 +28,10 @@ if (screen.width > 620) {
             markers: false,
             start: 'top bottom',
             end: 'bottom top',
-            scrub: 25,
+            scrub: 5,
             pin: false
         }
     });
-
-    tlSubBanner.from(".sub-banner",{
-        opacity:'0',
-        duration:0.1,
-    },"<")
 
     tlSubBanner.to(".projects-title-1",{
         // opacity:'0',
@@ -143,39 +66,39 @@ if (screen.width > 620) {
             markers: false,
             start: 'top bottom',
             end: 'bottom bottom',
-            scrub: 25,
+            scrub: 10,
             pin: false
         }
     });
 
     tlBe4care.from(".gsap-2",{
         opacity:'0',
-        x:'-50vw',
+        x:'50vw',
         duration:1,
     },"<")
 
     tlBe4care.from(".card-portafolio-be4care-1",{
         opacity:'0',
-        y:'-10vh',
+        x:'-50vw',
         duration:4,
         delay:1,
     },"<")
 
     tlBe4care.from(".card-portafolio-be4care-2",{
         opacity:'0',
-        y:'-25vh',
+        x:'-50vw',
         duration:4,
     },"<")
 
     tlBe4care.from(".card-portafolio-be4care-3",{
         opacity:'0',
-        y:'-10vh',
+        x:'-50vw',
         duration:4,
     },"<")
 
     tlBe4care.from(".card-portafolio-be4care-4",{
         opacity:'0',
-        y:'-25vh',
+        x:'-50vw',
         duration:4,
     },"<")
 
@@ -187,27 +110,27 @@ if (screen.width > 620) {
             markers: false,
             start: 'top bottom',
             end: 'bottom bottom',
-            scrub: 25,
+            scrub: 10,
             pin: false
         }
     });
 
     tlBe4tech.from(".gsap-3",{
         opacity:'0',
-        x:'-50vw',
+        x:'50vw',
         duration:1,
     },"<")
 
     tlBe4tech.from(".card-portafolio-be4tech-1",{
         opacity:'0',
-        y:'-10vh',
+        x:'-50vw',
         duration:2,
         delay:0.5,
     },"<")
 
     tlBe4tech.from(".card-portafolio-be4tech-2",{
         opacity:'0',
-        y:'-30vh',
+        x:'-50vw',
         duration:2,
     },"<")
 
@@ -220,27 +143,27 @@ if (screen.width > 620) {
             markers: false,
             start: 'top bottom',
             end: 'bottom bottom',
-            scrub: 25,
+            scrub: 10,
             pin: false
         }
     });
 
     tlIrocket.from(".gsap-4",{
         opacity:'0',
-        x:'-50vw',
+        x:'50vw',
         duration:1,
     },"<")
 
     tlIrocket.from(".card-portafolio-irocket-1",{
         opacity:'0',
-        y:'-10vh',
+        x:'-50vw',
         duration:2,
         delay:0.5,
     },"<")
 
     tlIrocket.from(".card-portafolio-irocket-2",{
         opacity:'0',
-        y:'-30vh',
+        x:'-50vw',
         duration:2,
     },"<")
 
@@ -251,20 +174,20 @@ if (screen.width > 620) {
             markers: false,
             start: 'top bottom',
             end: 'bottom bottom',
-            scrub: 25,
+            scrub: 10,
             pin: false
         }
     });
 
     tlQrAccess.from(".gsap-5",{
         opacity:'0',
-        x:'-50vw',
+        x:'50vw',
         duration:1,
     },"<")
 
     tlQrAccess.from(".card-portafolio-qr-access-1",{
         opacity:'0',
-        y:'-30vh',
+        x:'-50vw',
         duration:2,
         delay:0.5,
     },"<")
